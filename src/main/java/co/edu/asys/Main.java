3c97411da68b216ae222b92e30bf1d9b41b01b42
vera916developer <150732290+vera916developer@users.noreply.github.com>
2024-03-02 12:40:02 -0500

    Nueva clase main

--- src/main/java/co/edu/asys/Main.java
+++ src/main/java/co/edu/asys/Main.java
@@ -1,2 +1,11 @@
-package co.edu.asys;public class Main {
+package co.edu.asys;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+    }
+
 }
